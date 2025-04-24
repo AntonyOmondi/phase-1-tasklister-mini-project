d369592d76e63978e67909671f1e41434a7af017
Antony Omondi <tonymboya13@gmail.com>
2025-04-24 22:01:58 +0300

    added a handleToDo function that handles the to do added and displays it on the My Todos section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    handleToDo(e.target.new_task_description.value)
+  })
 });
+
+
+function handleToDo(todo){
+  let li = document.createElement("li")
+  li.textContent = todo
+  const ul = document.querySelector('ul')
+  ul.appendChild(li)
+}
